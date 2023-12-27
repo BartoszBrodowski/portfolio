c0686a7fecd7b59ef8888da66b3fba5a191da742
BartoszBrodowski <bartolini10058@gmail.com>
2023-12-27 12:17:19 +0100

    feat: theme provider and toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import ThemeToggle from '@/components/ThemeProvider/ThemeToggle'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <ThemeToggle />
     </main>
   )
 }
