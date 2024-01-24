0cd9fa92c8cd7d69b2843bab85595826511e11d5
Bartosz Brodowski <bartolini10058@gmail.com>
2024-01-24 22:37:58 +0100

    fix: delete strong tag from accordion

--- src/components/AboutMe/HobbiesAccordion.tsx
+++ src/components/AboutMe/HobbiesAccordion.tsx
@@ -9,23 +9,23 @@ const HobbiesAccordion: FunctionComponent = () => {
 			<AccordionItem value='item-1'>
 				<AccordionTrigger className='font-normal'>Formula 1</AccordionTrigger>
 				<AccordionContent>
-					<strong>Formula 1 🏁</strong> became my big interest back in 2021 and ever since I've been
-					a big fan of the sport. My favorite track is Austria, I currently dont support any
-					specific team/driver and I'm looking forward to {nextYear} season!
+					Formula 1 🏁 became my big interest back in 2021 and ever since I've been a big fan of the
+					sport. My favorite track is Austria, I currently dont support any specific team/driver and
+					I'm looking forward to {nextYear} season!
 				</AccordionContent>
 			</AccordionItem>
 			<AccordionItem value='item-2'>
 				<AccordionTrigger className='font-normal'>Guitar</AccordionTrigger>
 				<AccordionContent>
-					<strong>Guitar 🎸</strong> is a from time to time thing for me as of late. I play electric
-					guitar, mainly fingerstyle songs.
+					Guitar 🎸 is a from time to time thing for me as of late. I play electric guitar, mainly
+					fingerstyle songs.
 				</AccordionContent>
 			</AccordionItem>
 			<AccordionItem value='item-3'>
 				<AccordionTrigger className='font-normal'>Crocheting</AccordionTrigger>
 				<AccordionContent>
-					<strong>Crocheting 🧶</strong> is my latest hobby. I mainly crochet "Amigurumis" which are
-					little mascots made of fluffy yarn.
+					Crocheting 🧶 is my latest hobby. I mainly crochet "Amigurumis" which are little mascots
+					made of fluffy yarn.
 				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
